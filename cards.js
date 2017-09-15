bba22a665e3a63d0f3f9fc11eb5b54c17ee3cfe8
Personinous Apraham <thepeanutman98@gmail.com>
2017-09-15 08:14:11 -0400

    Fixing typo

--- cards.js
+++ cards.js
@@ -494,7 +494,7 @@ canvas.addEventListener("mousedown", function(e) {
        * True if this is a double click, false if not.
        * @type {Boolean}
        */
-      doubleClick: e.timeStamp - lastDragged.timeStamp < 500 && card === lastDragged.card
+      doubleClick: e.timeStamp - lastDragged.timeStamp < 500 && card == lastDragged.card
     };
     console.log(dragging); // Logs the value for reference
     /** @todo Add better logic probably for left/right mouse detection. The division -> boolean method works but is probably not the best */
@@ -551,7 +551,7 @@ canvas.addEventListener("mousedown", function(e) {
          * True if this is a double click, false if not.
          * @type {Boolean}
          */
-        doubleClick: e.timeStamp - lastDragged.timeStamp < 500 && card === lastDragged.card
+        doubleClick: e.timeStamp - lastDragged.timeStamp < 500 && stack == lastDragged.card
       };
       console.log(dragging); // Logs Stack if found otherwise undefined for reference
       stack.flipped = Boolean(e.button / 2); // e.button will be 0 if left mouse click and 2 if right mouse click, so dividing by 2 makes it 0 if left click 1 if right click, and then converts it to a Boolean so flipped is false if left click and true if right click
